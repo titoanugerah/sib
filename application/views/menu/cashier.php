b886aeb29e89aae339089405c5e54a93b915ab1a
titoanugerah <titoanugerah@student.undip.ac.id>
2021-08-29 13:14:18 +0700

    Added menu cashier

---
+++ application/views/menu/cashier.php
@@ -0,0 +1,13 @@
+<li class="nav-item active" >
+  <a href="<?php echo base_url('dashboard'); ?>">
+    <i class="fas fa-home"></i>
+    <p>Dashboard</p>
+  </a>
+</li>
+
+<li class="nav-item active" >
+  <a href="<?php echo base_url('transaction'); ?>">
+    <i class="fas fa-wrench"></i>
+    <p>Transaksi</p>
+  </a>
+</li>
